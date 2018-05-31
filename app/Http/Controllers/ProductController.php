ca6d976611ec63917d5ea262de3ea5148f96c401
Ali Yisa <aliyisa@outlook.com>
2018-06-01 00:49:36 +0100

    non-constrained categories

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use App\Brand;
 use App\Category;
 use App\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 /**
  * @resource Product
@@ -41,13 +42,35 @@ class ProductController extends Controller
         $this->validate($request,[
             'name' => 'required|string',
             'description' => 'required|string',
-            'code' => 'required|string',
             'price' => 'required|string',
             'quantity' => 'required|integer',
-            'brand_id' => 'required|integer',
-            'category_id' => 'required|integer',
+            'brand' => 'required|string',
+            'category' => 'required|string',
         ]);
-        $product = Product::create($request->all());
+        $product = new Product();
+        $product->name = $request->get("name");
+        $product->description = $request->get("description");
+        $product->price = $request->get("price");
+        $product->quantity = $request->get("quantity");
+        $product->code = Str::random(5);
+        if (Brand::where("name",$request->get("brand"))->count() > 0){
+            $brand = Brand::where("name",$request->get("brand"))->first();
+            $product->brand_id = $brand->id;
+        } else{
+            $brand = new Brand(["name" => $request->get("brand")]);
+            $brand->save();
+            $product->brand_id = $brand->id;
+        }
+        if (Category::where("name",$request->get("category"))->count() > 0){
+            $category = Category::where("name",$request->get("category"))->first();
+            $product->category_id = $category->id;
+        } else{
+            $category = new Category(["name" => $request->get("category")]);
+            $category->save();
+            $product->category_id = $category->id;
+        }
+
+        $product->save();
         return response()->json(['product'=>$product, 'brand' => Brand::find($product->brand_id), 'category' => Category::find($product->category_id)])->setStatusCode(201,"Resource created");
     }
     public function show($id)
